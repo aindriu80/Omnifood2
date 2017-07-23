d955d29063dcba1ad2eae350b54ea58f0ba9b038
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-07-23 16:24:13 +0100

    Scroll Animations continued - quicker times, all of animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -69,4 +69,29 @@ $('.js--section-features').waypoint(function(direction){
     },{
         offset: '50%'
     });
+
+    /* pulse animation */
+       $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    });
+
+
+       $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+
+
+
+       $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset: '50%'
+    });
+
+
+
 });
